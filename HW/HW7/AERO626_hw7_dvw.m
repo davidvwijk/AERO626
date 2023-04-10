51b3469585a33eea5b73df36f2464cf9d9d09b23
Davidvw22 <dev37@cornell.edu>
2023-04-09 19:28:03 -0500

    HW7 first push

---
+++ HW/HW7/AERO626_hw7_dvw.m
@@ -0,0 +1,15 @@
+%% AERO 626 Homework 7
+%
+%   Texas A&M University
+%   Aerospace Engineering
+%   van Wijk, David
+
+close all; clear; clc;
+plot_flag = true;
+
+% Seed to reproduce results
+rng(10)
+
+xaxis_sz = 20; yaxis_sz = 20; legend_sz = 18;
+
+%% Part A - Initialization and Data Processing
