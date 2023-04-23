3044630a6855fdbb569645dd7a6ffc46e342852a
Davidvw22 <dev37@cornell.edu>
2023-04-23 17:20:34 -0500

    HW8 commit

---
+++ HW/HW8/AERO626_hw8_dvw.m
@@ -0,0 +1,35 @@
+%% AERO 626 Homework 8
+%
+%   Texas A&M University
+%   Aerospace Engineering
+%   van Wijk, David
+
+close all; clear; clc;
+
+plot_flag = true;
+
+xaxis_sz = 20; yaxis_sz = 20; legend_sz = 18;
+
+%% Part 1 - EKF Implementation
+
+
+
+%% Part 2 - Bootstrap Particle Filter
+
+
+
+%% Part 3 - Bootstrap Particle Filter with Resampling
+
+
+
+%% Part 4 - Filter Comparison
+
+
+
+%% Computation Functions
+
+
+
+%% Plotting Functions
+
+
